3bb867c6e6775001a01e6c41ab45330a75ff1e6f
yuri <yuriVlopes@outlook.com>
2022-12-01 14:30:57 -0300

    Criando página de detalhes do evento

--- src/main/java/atividades/pi/atividades/controllers/AtvformController.java
+++ src/main/java/atividades/pi/atividades/controllers/AtvformController.java
@@ -1,10 +1,12 @@
 package atividades.pi.atividades.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,28 +19,44 @@ import atividades.pi.atividades.repositories.EventoRepository;
 public class AtvformController {
 
 	@Autowired
-	private  EventoRepository er;
-	
+	private EventoRepository er;
+
 	@GetMapping("/atividades/form")
 	public String form() {
 		return "eventos/formAtv";
 	}
-	
+
 	@PostMapping
 	public String adicionar(atv302 atv302) {
-		
+
 		System.out.println(atv302);
 		er.save(atv302);
-		
+
 		return "eventos/atv-adicionado";
 	}
-	
+
 	@GetMapping
 	public ModelAndView listar() {
-		
+
 		List<atv302> eventos = er.findAll();
 		ModelAndView mv = new ModelAndView("eventos/lista");
 		mv.addObject("eventos", eventos);
 		return mv;
+	}
+
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		
+		ModelAndView md = new ModelAndView();
+		Optional<atv302> opt = er.findById(id);
+		if(opt.isEmpty()) {
+			md.setViewName("redirect:/eventos");
+			return md;
+		}
+		md.setViewName("eventos/detalhes");
+		atv302 evento = opt.get();
+		md.addObject("evento", evento);
+		
+		return md;
 	}
 }
